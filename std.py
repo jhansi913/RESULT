46678357be946c2f62867a9c5594efa34c0a6d62
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-05-07 19:20:34 +0530

    std.py

--- std.py
+++ std.py
@@ -13,8 +13,9 @@ def get_student_result(df, roll_number):
     student_data = df[df['roll_number'] == roll_number]
     st.write(student_data)
     st.write(len(student_data))
+    return student_data.iloc[0].to_dict()  # Convert row to dictionary
  # Filter DataFrame by roll number
-   return student_data.iloc[0].to_dict()  # Convert row to dictionary
+    
    
 
 # Streamlit app title and description
